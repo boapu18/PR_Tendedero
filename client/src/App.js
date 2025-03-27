efa34df14d8501416e1b4d2a58cf2433daf73ff1
marcohg12 <marcohg0312@gmail.com>
2025-03-26 23:13:41 -0600

    enrutamiento en react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
 import FormPage from "./pages/FormPage";
 import ReportVisualizer from "./pages/ReportVisualizer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <FormPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="home" element={<ReportVisualizer/>}></Route>
+          <Route path="report" element={<FormPage/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/ClientNavBar.js
+++ client/src/components/ClientNavBar.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function NavBar(){
+
+    const navigate = useNavigate();
+
+    const handleRegisterButtonClick = () => {
+        navigate("/report");
+    };
 
     return (
         <div className="border-bottom border-1 border-dark sticky-top" style={{backgroundColor: "white"}}>
             <div className="d-flex justify-content-between align-items-center mx-2" style={{minHeight: "100px"}}>
                 <h1>Tendedero Virtual de Denuncia</h1>
-                <button className="main-button">Registrar denuncia</button>
+                <button className="main-button" onClick={handleRegisterButtonClick}>Registrar denuncia</button>
             </div>
         </div>
     );
