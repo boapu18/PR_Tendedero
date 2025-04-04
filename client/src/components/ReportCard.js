c926adfc58ee2aaafcc88953fd200148c265c34d
marcohg12 <marcohg0312@gmail.com>
2025-04-03 23:12:03 -0600

    fix en texto de cards

--- client/src/components/ReportCard.js
+++ client/src/components/ReportCard.js
@@ -5,7 +5,7 @@ function ReportCard({ report }){
     return (
         <div className="mx-3 p-4 report-card-bg">
             <div>
-                <p className="card-text">{ report.content }</p>
+                <p className="card-text text-wrap text-break">{ report.content }</p>
             </div>
         </div>
     );
