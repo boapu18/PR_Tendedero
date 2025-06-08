5bdd98349808beb210e4e7ec79591812bba31ce2
marcohg12 <marcohg0312@gmail.com>
2025-06-08 02:07:16 -0600

    manejo de errores en descarga de csv frontend

--- client/src/components/report/ExportDataButton.js
+++ client/src/components/report/ExportDataButton.js
@@ -1,20 +1,28 @@
 import React from "react";
 import axios from "axios";
+import { errorAlert } from "../../utils/alertInvokers";
+import { DEFAULT_ERROR_MESSAGE } from "../../utils/constants";
 
 const handleExportDataButtonClick = async () => {
 
-  const response = await axios.get(`${process.env.REACT_APP_API_URL}/reports/download-csv`, {
-    responseType: 'blob',
-    withCredentials: true
-  });
-
-  const url = window.URL.createObjectURL(new Blob([response.data]));
-  const link = document.createElement('a');
-  link.href = url;
-  link.setAttribute('download', 'Reportes.csv');
-  document.body.appendChild(link);
-  link.click();
-  link.remove();
+  try {
+    
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/reports/download-csv`, {
+      responseType: 'blob',
+      withCredentials: true
+    });
+  
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', 'Reportes.csv');
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    
+  } catch (e){
+    errorAlert(e.response?.data?.message ?? DEFAULT_ERROR_MESSAGE);
+  }
 };
 
 function ExportDataButton() {
